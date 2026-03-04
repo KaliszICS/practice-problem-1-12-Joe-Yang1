e2c6bcdbf7294d38b54c8be611d45ded979021fa
Joe-Yang1 <348993072@tdsb.ca>
2026-03-04 16:21:35 +0000

    Done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,3 +1,12 @@
+/**
+ * File: Converting Data Types
+ * Author: Joe Yang
+ * Date Created: Mar 4, 2026
+ * Date Last Modified: Mar 4, 2026
+ */
+
+
+import java.util.Scanner;
 public class PracticeProblem {
 
 	public static void main(String args[]) {
@@ -9,18 +18,42 @@ public class PracticeProblem {
 
 	public static void q1() {
 		//Write question 1 code here
+	Scanner input = new Scanner(System.in);
+	System.out.print("Input an integer: ");
+	int num = input.nextInt();
+	num = (num + 3);
+	System.out.println(num);
 	}
 
 	public static void q2() {
 		//Write question 2 code here
+	Scanner input = new Scanner(System.in);
+	System.out.print("Input a number: ");
+	String num1 = (input.nextLine() + "4");
+	double num2 = Double.parseDouble(num1);
+	System.out.println(num2 + 2);
+	
 	}
 
 	public static void q3() {
 		//Write question 3 code here
+	Scanner input = new Scanner(System.in);
+	System.out.print("Input a radius: ");
+	Double num = input.nextDouble();
+	Double pi =  (3.14);
+	System.out.println(pi * (num*num)); 
+
+	
 	}
 
 	public static void q4() {
 		//Write question 4 code here
+	Scanner input = new Scanner(System.in);
+	System.out.print("Input a number: ");
+	Double num = input.nextDouble();
+	num = (num*12);
+	System.out.println(Math.floor(num));
+
 	}
 
 }
